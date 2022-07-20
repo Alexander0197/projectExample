5ae3ac71868f4152cd91185c7d411efc3a464d4b
Alexander Azakevich <alexander.azar97@gmail.com>
2022-07-20 16:09:11 +0300

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.model.City;
+import com.example.demo.repository.CityRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        CityRepository repository = context.getBean(CityRepository.class);
+
+        //crud
+        //create
+//        repository.save(new City("Minsk"));
+//        repository.save(new City("Piter"));
+//        repository.save(new City("Moscow"));
+//        repository.save(new City("LA"));
+//        List<City> cities = repository.findAll();
+//        System.out.println(cities);
+//        repository.save( new City(4, "Los Angeles"));
+//      repository.deleteById(6);
+//      repository.deleteById(5);
+//       repository.save(new City(4, "Los Angeles"));
+//        repository.delete(new City(4));
+    }
 
 }
---
+++ src/main/java/com/example/demo/model/City.java
@@ -0,0 +1,98 @@
+package com.example.demo.model;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    private int population;
+
+    private boolean isTouristic;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "city")
+    private List<Person> people;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date foundationDate;
+
+    public City() {
+    }
+
+    public City(int id) {
+        this.id = id;
+    }
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public City(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public boolean isTouristic() {
+        return isTouristic;
+    }
+
+    public void setTouristic(boolean touristic) {
+        isTouristic = touristic;
+    }
+
+    public Date getFoundationDate() {
+        return foundationDate;
+    }
+
+    public void setFoundationDate(Date foundationDate) {
+        this.foundationDate = foundationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
